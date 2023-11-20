faefba0b16a8b178dfbf05523a52466de77fbe07
jihyunl2 <lea@floatic.io>
2023-11-20 11:46:28 +0900

    vertex load with correct offset

--- topology_web_editor/frontend/editor.js
+++ topology_web_editor/frontend/editor.js
@@ -1,31 +1,30 @@
 function loadMarkers(vertices) {
     const markerContainer = document.getElementById('markerContainer');
-    const image = document.querySelector('img[src="map.png"][alt="Navigation Map"]');
-    const imageRect = image.getBoundingClientRect();
-    const imageX = imageRect.left;
-    const imageY = imageRect.top;
+    const imageRect = markerContainer.getBoundingClientRect();
+    const imageLeft = imageRect.left;
+    const imageBottom = imageRect.bottom;
 
     vertices.forEach(vertex => {
         const marker = document.createElement('div');
         marker.className = 'marker';
         marker.id = vertex.id;
-        const xOnImage = -imageX + vertex.x;
-        const yOnImage = -imageY + vertex.y;
+        const xOnImage = imageLeft + vertex.x;
+        const yOnImage = imageBottom + vertex.y;
         marker.style.left = xOnImage + 'px';
-        marker.style.top = yOnImage + 'px';
+        marker.style.bottom = yOnImage + 'px';
 
         // Make the marker draggable using interact.js
         interact(marker)
             .draggable({
                 onmove: (event) => {
                     const target = event.target;
-                    const x = (parseFloat(target.style.left) || 0) + event.dx;
-                    const y = (parseFloat(target.style.top) || 0) + event.dy;
+                    const x = (parseFloat(target.style.left)) + event.dx;
+                    const y = (parseFloat(target.style.bottom)) + event.dy;
 
                     target.style.left = x + 'px';
-                    target.style.top = y + 'px';
+                    target.style.bottom = y + 'px';
 
-                    updateVertexPosition(target);
+                    updateVertexPosition(target, event.dx, event.dy);
                 }
             });
 
@@ -34,22 +33,12 @@ function loadMarkers(vertices) {
 }
 
 // Create function to send vertex position (pixel coordinates) updates to server
-function updateVertexPosition(marker) {
-    const x = parseFloat(marker.style.left) || 0;
-    const y = parseFloat(marker.style.top) || 0;
-    const image = document.querySelector('img[src="map.png"][alt="Navigation Map"][id="mapImage"]');
-    const imageRect = image.getBoundingClientRect();
-    const imageX = imageRect.left;
-    const imageY = imageRect.top;
-
-    marker.className = 'marker';
-    const xOnMap = x + imageX;
-    const yOnMap = y + imageY;
+function updateVertexPosition(marker, dx, dy) {
 
     const requestData = {
         'id': marker.id,  // You can add an id to each marker for identification
-        'x': xOnMap,
-        'y': yOnMap,
+        'dx': dx,
+        'dy': dy,
     };
 
     fetch('http://127.0.0.1:5000/update_vertex_position', {
