7ede67662cefda8fa4c5ef69117d93c9970f4d89
jihyunl2 <lea@floatic.io>
2023-12-12 14:31:35 +0900

    fix script

--- topology_web_editor/run.sh
+++ topology_web_editor/run.sh
@@ -1,5 +1,26 @@
 #!/bin/bash
-python3 backend/app.py &
 
+# Function to handle cleanup on exit
+cleanup() {
+  echo "Cleaning up..."
+  # Kill the background processes
+  kill -s SIGTERM $http_server_pid $flask_app_pid
+  exit 0
+}
+
+# Trap Ctrl+C to call the cleanup function
+trap cleanup INT
+
+# Run the Flask app
+cd backend
+python3 app.py &
+flask_app_pid=$!
+cd ..
+
+# Run the HTTP server
 cd frontend
-python3 -m http.server --bind 127.0.0.1
+python3 -m http.server --bind 127.0.0.1 &
+http_server_pid=$!
+
+# Wait for the background processes to finish
+wait $flask_app_pid $http_server_pid || true
